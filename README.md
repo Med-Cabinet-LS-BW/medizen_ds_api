852e082f9eef1fa843d16e9a642b8ee1211c15c4
tobias-fyi <tobyreaper@gmail.com>
2019-11-18 17:06:04 -0700

    Built initial Flask API routes

--- README.md
+++ README.md
@@ -1,3 +1,57 @@
 # MediZen Recommendation
 
 A simple back-end Flask API to recommend cannabis strains in the MediZen app.
+
+- [MediZen Recommendation](#medizen-recommendation)
+  - [Usage](#usage)
+    - [All Strains](#all-strains)
+      - [Example Strains Request](#example-strains-request)
+    - [Recommended Strains](#recommended-strains)
+      - [Example Recommendation Request](#example-recommendation-request)
+  - [Project Information](#project-information)
+    - [Data](#data)
+
+## Usage
+
+### All Strains
+
+Endpoint to return a complete list of strains.
+
+    /strains
+
+Parameters: None
+
+Returns: JSON array containing all available strains.
+
+#### Example Strains Request
+
+    medizen.io/api/strains
+
+### Recommended Strains
+
+Endpoint to return a list of recommendations.
+
+    /rec/<effects>
+
+Parameters: Effects: list of desired effects, comma-delimited.
+
+Returns: JSON array of top n recommendations.
+
+#### Example Recommendation Request
+
+    medizen.io/api/rec/Uplifted,Happy,Relaxed,Energetic
+
+---
+
+## Project Information
+
+### Data
+
+[Link to Dataset](https://www.kaggle.com/kingburrito666/cannabis-strains)
+
+The recommendation API uses the Cannabis Strains dataset, uploaded to Kaggle.
+
+We used K-Means Clustering to create 5 categories using the effects and description of
+each strain.
+
+Next we pickled a vectorizer and KNN model to create an API that output the top 5 recommendations.
--- app.py
+++ app.py
@@ -5,8 +5,8 @@ A simple back-end Flask API for recommending cannabis strains.
 """
 
 from flask import Flask
-
-# from flask_sqlalchemy import SQLAlchemy
+import pickle 
+import pandas as pd
 
 import os
 from dotenv import load_dotenv
@@ -14,11 +14,98 @@ from dotenv import load_dotenv
 load_dotenv()
 
 app = Flask(__name__)
-# app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///db.sqlite3"
-# db = SQLAlchemy(app)
 
 
-@app.route("/rec")
-def rec():
-    """Primary recommendation route."""
-    pass
+# Load in the dataset
+df = pd.read_csv("data/cannabis.csv")
+
+# Extract index as column
+df2 = df.reset_index()
+
+# Load in the pickles
+tfidf = pickle.load(open("data/vect_01.pkl", "rb"))
+nn = pickle.load(open("data/knn_01.pkl", "rb"))
+
+
+def recommend(request, n=5):
+    """
+    Creates a dataframe with top n recommended strains.
+    
+    Parameters
+    ----------
+    request : string
+        List of user's desired effects, concatenated into a single string.
+        Separated by commas.
+    n : int, optional
+        Number of recommendations to return, by default 5
+    
+    Returns
+    -------
+    recs
+        Returns a list of recommended strains.
+    """
+
+    # Transform
+    request = pd.Series(request)
+    request_sparse = tfidf.transform(request)
+
+    # Send to df
+    request_tfidf = pd.DataFrame(request_sparse.todense())
+
+    # Return a list of indexes
+    top5 = nn.kneighbors([request_tfidf][0], n_neighbors=5)[1][0].tolist()
+    
+    # Send recomendations to DataFrame
+    recs_df = df.iloc[top5].reset_index()
+
+    # Extract pd.Series of only "Strain"
+    recs_dict = recs_df[["index", "Strain"]].to_json(orient="records")
+    
+    return recs_dict
+
+# TODO: Determine if `n` should be route parameter
+@app.route("/rec/<effects>")
+def rec(effects):
+    """
+    Primary recommendation route.
+    
+    Parameters
+    ----------
+    effects : string
+        List of desired effects, comma-delimited.
+
+    Returns
+    -------
+    top : JSON
+        Returns a JSON array of top n recommendations.
+    """
+
+    try:
+        top = recommend(effects)
+    except Exception as e:
+        raise e
+        top = "There was an error with the request."
+
+    return str(top)
+
+
+@app.route("/strains")
+def strains():
+    """
+    Endpoint that returns a list of all available strains.
+
+    Returns
+    -------
+    strains : JSON
+        Returns a JSON array of all available strains.
+    """
+
+    try:
+        # TODO: Return index+name or just index
+        strains = df2[["index", "Strain"]].to_json(orient="records")
+    except Exception as e:
+        raise e
+        strains = "There was an error with the request."
+
+    return strains
+
